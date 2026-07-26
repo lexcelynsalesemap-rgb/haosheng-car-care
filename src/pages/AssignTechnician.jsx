fec1b0f45e0fac24e5e88af6b92302cab8a91b32
Haosheng Car Care <lexcelyn.salesemap@gmail.com>
2026-07-26 15:32:13 +0300

    Add Assign Technician page

---
+++ src/pages/AssignTechnician.jsx
@@ -0,0 +1,21 @@
+function AssignTechnician(){
+
+return (
+
+<div>
+
+<h1>
+Assign Technician
+</h1>
+
+<p>
+Technician assignment page
+</p>
+
+</div>
+
+);
+
+}
+
+export default AssignTechnician;
